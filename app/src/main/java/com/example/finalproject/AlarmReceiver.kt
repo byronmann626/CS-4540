58420558dada72fe38d92834ad07339de41ccfc8
dan2430 <dan2430@gmail.com>
2019-12-09 18:24:36 -0800

    merged vincent and daniels part

---
+++ app/src/main/java/com/example/finalproject/AlarmReceiver.kt
@@ -0,0 +1,75 @@
+package com.example.finalproject
+
+
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Context.CONTEXT_IGNORE_SECURITY
+import android.content.Context.NOTIFICATION_SERVICE
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
+
+class AlarmReceiver : BroadcastReceiver() {
+    private lateinit var context:Context
+    override fun onReceive(context: Context, intent: Intent) {
+        this.context=context
+        // Is triggered when alarm goes off, i.e. receiving a system broadcast
+        if (intent.action == "FOO_ACTION") {
+            val fooString = intent.getStringExtra("KEY_FOO_STRING")
+            Toast.makeText(context, fooString, Toast.LENGTH_LONG).show()
+            Log.d("print","BROADCAST RECEIVER_+_+_+_+_+_")
+            val channelId =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    createNotificationChannel("24", "Alarm Channel")
+                } else {
+                    // If earlier version channel ID is not used
+                    // https://developer.android.com/reference/android/support/v4/app/NotificationCompat.Builder.html#NotificationCompat.Builder(android.content.Context)
+                    ""
+                }
+            val notificationIntent = Intent(context, MainActivity::class.java)
+            notificationIntent.action = "My action"  // A string containing the action name
+            val contentPendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0)
+            val notification = NotificationCompat.Builder(context,channelId)
+                .setContentTitle("TITLE")
+                .setTicker("TICKER")
+                .setContentText("THis is my content text")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(contentPendingIntent)
+                .setOngoing(true)
+
+
+
+
+
+            val service = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            service.notify(0, notification.build())
+
+
+
+        }
+
+
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String{
+        val chan = NotificationChannel(channelId,
+            channelName, NotificationManager.IMPORTANCE_HIGH)
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
+    }
+
+}
--- app/src/main/java/com/example/finalproject/SettingsFragment.kt
+++ app/src/main/java/com/example/finalproject/SettingsFragment.kt
@@ -1,13 +1,24 @@
 package com.example.finalproject
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import com.example.finalproject.databinding.FragmentSettingsBinding
 import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_settings.view.*
+
+import kotlinx.android.synthetic.main.fragment_settings.*
+import java.util.*
 
 
 /**
@@ -25,13 +36,47 @@ class SettingsFragment : Fragment() {
         binding.okButton.setOnClickListener{
                 view : View ->
             view.findNavController().navigate(R.id.action_settingsFragment_to_alarmFragment3)
+            val calendar = Calendar.getInstance()
+            val chips=binding.chipGroup.checkedChipId
+            Log.d("chips",""+chips)
+//            calendar.set(
+//                datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(),
+//                timePicker.getCurrentHour(), timePicker.getCurrentMinute(), 0
+//            )
+            val startTime = calendar.getTimeInMillis()
+
+
+            Toast.makeText(activity, binding.timePicker1.hour.toString()+" "+binding.timePicker1.minute.toString() ,Toast.LENGTH_LONG).show()
+            createAlarmManager()
+
         }
+
         binding.cancelButton.setOnClickListener{
                 view : View ->
             view.findNavController().navigate(R.id.action_settingsFragment_to_alarmFragment3)
         }
 
         return binding.root
+    }
+    //
+    private fun createAlarmManager(){
+        val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+// Intent part
+        val intent = Intent(context, AlarmReceiver::class.java)
+        intent.action = "FOO_ACTION"
+        intent.putExtra("KEY_FOO_STRING", "Medium AlarmManager Demo")
+
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+
+// Alarm time
+        val ALARM_DELAY_IN_SECOND = 10
+        val alarmTimeAtUTC = System.currentTimeMillis() + ALARM_DELAY_IN_SECOND * 1_000L
+        Log.d("time",""+alarmTimeAtUTC)
+// Set with system Alarm Service
+// Other possible functions: setExact() / setRepeating() / setWindow(), etc
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, alarmTimeAtUTC, pendingIntent)
+
     }
 
 
