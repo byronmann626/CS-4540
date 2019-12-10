954f779db86698ebdd0a14c668a5267f38ad97da
dan2430 <dan2430@gmail.com>
2019-12-09 21:15:04 -0800

    combined front and backend, alarm setting works

--- app/src/main/java/com/example/finalproject/SettingsFragment.kt
+++ app/src/main/java/com/example/finalproject/SettingsFragment.kt
@@ -15,10 +15,13 @@ import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import com.example.finalproject.databinding.FragmentSettingsBinding
 import androidx.navigation.findNavController
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 import kotlinx.android.synthetic.main.fragment_settings.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 /**
@@ -37,17 +40,36 @@ class SettingsFragment : Fragment() {
                 view : View ->
             view.findNavController().navigate(R.id.action_settingsFragment_to_alarmFragment3)
             val calendar = Calendar.getInstance()
-            val chips=binding.chipGroup.checkedChipId
-            Log.d("chips",""+chips)
+            val chipgroup:ChipGroup=binding.chipGroup
+            val m=chipgroup.findViewById<Chip>(R.id.monday)
+            val t=chipgroup.findViewById<Chip>(R.id.tuesday)
+            val w=chipgroup.findViewById<Chip>(R.id.wednesday)
+            val th=chipgroup.findViewById<Chip>(R.id.thursday)
+            val f=chipgroup.findViewById<Chip>(R.id.friday)
+            val sat=chipgroup.findViewById<Chip>(R.id.saturday)
+            val sun=chipgroup.findViewById<Chip>(R.id.sunday)
+            var selectedDate=ArrayList<Boolean>()
+            selectedDate.add(m.isChecked)
+            selectedDate.add(t.isChecked)
+            selectedDate.add(w.isChecked)
+            selectedDate.add(th.isChecked)
+            selectedDate.add(f.isChecked)
+            selectedDate.add(sat.isChecked)
+            selectedDate.add(sun.isChecked)
+           var alarmTime= createDate(selectedDate,binding.timePicker1.hour,binding.timePicker1.minute)
+
+
+
+            Log.d("chips",""+selectedDate.toString())
 //            calendar.set(
 //                datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(),
 //                timePicker.getCurrentHour(), timePicker.getCurrentMinute(), 0
 //            )
-            val startTime = calendar.getTimeInMillis()
+
 
 
             Toast.makeText(activity, binding.timePicker1.hour.toString()+" "+binding.timePicker1.minute.toString() ,Toast.LENGTH_LONG).show()
-            createAlarmManager()
+            createAlarmManager(alarmTime)
 
         }
 
@@ -59,7 +81,7 @@ class SettingsFragment : Fragment() {
         return binding.root
     }
     //
-    private fun createAlarmManager(){
+    private fun createAlarmManager(alarmTime:Long){
         val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
 // Intent part
@@ -75,8 +97,38 @@ class SettingsFragment : Fragment() {
         Log.d("time",""+alarmTimeAtUTC)
 // Set with system Alarm Service
 // Other possible functions: setExact() / setRepeating() / setWindow(), etc
-        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, alarmTimeAtUTC, pendingIntent)
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent)
 
+    }
+
+    private fun createDate(days:ArrayList<Boolean>, hour:Int, minute:Int): Long {
+        //0=monday 1= tuesday 2= wednesday etc
+        var day=days.indexOf(true)
+        val calendar=Calendar.getInstance().time
+
+        Log.d("debug",calendar.toString())
+        var currDate:Int=0
+        if(calendar.toString().equals("Mon"))
+            currDate=0
+        if(calendar.toString().equals("Tue"))
+            currDate=1
+        if(calendar.toString().equals("Wed"))
+            currDate=2
+        if(calendar.toString().equals("Thu"))
+            currDate=3
+        if(calendar.toString().equals("Fri"))
+            currDate=4
+        if(calendar.toString().equals("Sat"))
+            currDate=5
+        if(calendar.toString().equals("Sun"))
+            currDate=6
+
+        var offset=(day-currDate)
+        Log.d("debug","offset "+offset+" ")
+
+        var alarmDate=Date(calendar.year,calendar.month,calendar.date+offset,hour,minute)
+        Log.d("debug",""+alarmDate.time)
+        return alarmDate.time
     }
 
 
